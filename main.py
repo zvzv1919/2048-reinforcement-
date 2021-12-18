d94b3ac9f792d4681aa10c693177d6880bd826b2
Kaiyuan Hou <86454568+Hou-Kaiyuan@users.noreply.github.com>
2021-12-18 14:44:27 -0500

    update

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import randommodel
 import greedy_model
 import mcts_model_vanilla
 import mcts_model_table
+import dqn_model
 import matplotlib.pyplot as plt
 
 
 
 
 num_runs = 50
-model = mcts_model_table
+model = dqn_model
 
 def plot_perf(data):
     fig, ax = plt.subplots()
@@ -25,21 +26,28 @@ def plot_perf(data):
 
 if __name__ == '__main__':
   model_name = model.name
-  model = model.Model()
   env = Game2048Env()
-  model.train(env, load="model 7M", save="model 9M")
+  model = model.Model(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01,
+                  input_dims=[env.observation_space.shape[-1]], lr=0.001)
+  
+  # model.train(env, load="model 3M", save="model 5M")
 
   # env.render()
   moves_arr = []
   score_arr = []
 
   for i in range(num_runs):
-    env.reset()
+    observation,_,_,_  = env.reset()
+    observation = observation.reshape(-1)
     done = False
     while not done:
-      action = model.predict(env)
-      next_state, reward, done, info = env.step(action)
-
+      action = model.predict(observation)
+      observation_, reward, done, info = env.step(action)
+      observation_ = observation_.reshape(-1)
+      model.store_transition(observation, action, reward,
+                                    observation_, done)
+      model.learn()
+      observation = observation_      
       # print('Next Action: "{}"\n\nReward: {}'.format(gym_2048.Base2048Env.ACTION_STRING[action], reward))
       # env.render()
     moves_arr.append(env.steps)
@@ -47,11 +55,12 @@ if __name__ == '__main__':
     env.render()
 
     print('\nTotal Moves: {}'.format(moves_arr[-1]))
-    print(np.max(next_state))
-    print("mem len:", len(model.mem))
+    print(np.max(observation_))
+    # print("mem len:", len(model.mem))
   print("Average Moves for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(moves_arr)/len(moves_arr)))
   print("Average Score for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(score_arr)/len(score_arr)))
   plot_perf(moves_arr)
+
 
 
 
