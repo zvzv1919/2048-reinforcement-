f168fba4bb56730b4b0e463ca323acefcdfcbbce
zvzv1919 <zvzv1919@gmail.com>
2021-12-19 15:08:52 -0500

    model adjust
    Adjusted mcts table model, now the score should equal to the expected moves till end. Also adjusted parameters to make the model better. Added log print function in main.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import greedy_model
 import mcts_model_vanilla
 import mcts_model_table
 import matplotlib.pyplot as plt
-
+import json
 
 
 
@@ -23,35 +23,56 @@ def plot_perf(data):
     ax.set_ylabel('moves')
     plt.show()
 
+def print_log(logfile):
+    f = open(logfile, 'r')
+    dict_ = json.load(f)
+    f.close()
+    fig, ax = plt.subplots()
+    data = dict_["moves_arr"]
+    data_10ma = []
+    for i in range(len(data) // 10):
+        data_10ma.append(np.average(data[i:i+10]))
+
+    x = np.linspace(0,len(data), len(data_10ma))
+    ax.plot(x, data_10ma, linewidth=2.0)
+
+    ax.set_xlabel('run')
+    ax.set_ylabel('moves')
+    plt.show()
+
 if __name__ == '__main__':
-  model_name = model.name
-  model = model.Model()
-  env = Game2048Env()
-  model.train(env, load="model 7M", save="model 9M")
+  print_log("training_log:model 1M")
+  # model_name = model.name
+  # model = model.Model()
+  # env = Game2048Env()
+  # env.reset()
+  #
+  # model.train(env, load=None, save="model 1M")
+  #
+  # # env.render()
+  # moves_arr = []
+  # score_arr = []
+  #
+  # for i in range(num_runs):
+  #   env.reset()
+  #   done = False
+  #   while not done:
+  #     action = model.predict(env)
+  #     next_state, reward, done, info = env.step(action)
+  #
+  #     # print('Next Action: "{}"\n\nReward: {}'.format(gym_2048.Base2048Env.ACTION_STRING[action], reward))
+  #     # env.render()
+  #   moves_arr.append(env.steps)
+  #   score_arr.append(env.score)
+  #   env.render()
+  #
+  #   print('\nTotal Moves: {}'.format(moves_arr[-1]))
+  #   print(np.max(next_state))
+  #   print("mem len:", len(model.mem))
+  # print("Average Moves for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(moves_arr)/len(moves_arr)))
+  # print("Average Score for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(score_arr)/len(score_arr)))
+  # plot_perf(moves_arr)
 
-  # env.render()
-  moves_arr = []
-  score_arr = []
-
-  for i in range(num_runs):
-    env.reset()
-    done = False
-    while not done:
-      action = model.predict(env)
-      next_state, reward, done, info = env.step(action)
-
-      # print('Next Action: "{}"\n\nReward: {}'.format(gym_2048.Base2048Env.ACTION_STRING[action], reward))
-      # env.render()
-    moves_arr.append(env.steps)
-    score_arr.append(env.score)
-    env.render()
-
-    print('\nTotal Moves: {}'.format(moves_arr[-1]))
-    print(np.max(next_state))
-    print("mem len:", len(model.mem))
-  print("Average Moves for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(moves_arr)/len(moves_arr)))
-  print("Average Score for {runs} runs with {model_name}: {moves}".format(model_name = model_name, runs = num_runs, moves = sum(score_arr)/len(score_arr)))
-  plot_perf(moves_arr)
 
 
 
--- mcts_model_table.py
+++ mcts_model_table.py
@@ -10,24 +10,23 @@ import json
 
 name = 'mcts model table'
 
-explore_width = 10
-explore_depth = 20
-training_episode = 600
-learning_rate = 0.3
+explore_width = 20
+explore_depth = 35
+training_episode = 500
+learning_rate = 0.2
 action_space = [0,1,2,3]
 
 class Model():
     def __init__(self):
         #TODO: make this LRU_cache
         self.mem = collections.defaultdict(int)
-
+        self.N = collections.defaultdict(int)
 
     def predict(self, env):
         action_values = [0,0,0,0]
-        lock = threading.Lock()
         thread_pool = []
         for action in action_space:
-            thread_pool.append(threading.Thread(target = evaluate_action, args = (self.mem, env, action, action_values, lock)))
+            thread_pool.append(threading.Thread(target = evaluate_action, args = (self.mem, self.N, env, action, action_values)))
             thread_pool[-1].start()
         for t in thread_pool:
             t.join()
@@ -73,7 +72,7 @@ class Model():
         self.mem = collections.defaultdict(int, json.load(f))
         f.close()
 
-def evaluate_action(mem, env, action, action_values, lock):
+def evaluate_action(mem, N, env, action, action_values):
     # print("jaja, {}".format(env.steps))
     virtual_env = gym_2048.Game2048Env()
     scores = []
@@ -87,88 +86,88 @@ def evaluate_action(mem, env, action, action_values, lock):
         illegal_actions = set() #Used to remember illegal actions for the current exploration step
 
         # The first action would be the one specified
-        _, reward, done, _ = virtual_env.step(action)
+        next_state, reward, done, _ = virtual_env.step(action)
 
         # If action is illegal, set value to 0
         if reward == -1:
             action_values[action] = 0
             return
 
-        while virtual_env.steps < explore_depth:
-            if done:
-                break
-            state = mat_to_hash(virtual_env.get_board())
+        while virtual_env.steps <= explore_depth and not done:
 
+            state = mat_to_hash(next_state)
             # if legal move, append the new state to the state sequence
             if state != states[-1]:
                 states.append(state)
 
             # Randomly choose successive action in explore mode
             explore_action = np.random.randint(0, 4)
-
             # If the previous move is illegal, try to choose between legal moves
-            if reward == env.illegal_move_reward:
+            if reward == virtual_env.illegal_move_reward:
                 while explore_action in illegal_actions:
                     explore_action = np.random.randint(0, 4)
 
-            _, reward, done, _ = virtual_env.step(explore_action)
-
+            next_state, reward, done, _ = virtual_env.step(explore_action)
             if reward == env.illegal_move_reward:
                 illegal_actions.add(explore_action)
             else:
                 illegal_actions = set()
 
-        score = mem[mat_to_hash(virtual_env.get_board())]
-        if not done and score == 0:
-            # initialize current state score to non-zero value
-            mem[mat_to_hash(virtual_env.get_board())]=explore_depth*0.2
-            score = explore_depth*0.2
+        score = mem[mat_to_hash(next_state)]
+        # if not done and score == 0:
+        #     # initialize current state score to non-zero value
+        #     mem[mat_to_hash(next_state)] = 3
+        #     score = 3
 
         # Back propagates reward to all states visited in this trial
         for state in states[::-1]:
-            mem[state] += learning_rate * (score - mem[state])
             score += 1
+            mem[state] = (mem[state] * N[state] + score) / (N[state] + 1)
+            N[state] += 1
+            # if mem[state] == 0:
+            #     mem[state] = score
+            # else:
+            #     mem[state] = mem[state] + learning_rate * (score - mem[state])
+
         # Trial over, use the score of first state in this trial as the trial score
-        scores.append(mem[states[0]])
+        if len(states) == 1:
+            scores.append(1)
+        else:
+            scores.append(1 + mem[states[1]])
     # print(action, scores)
 
-    # lock.acquire()
-    # print(sum(scores) / len(scores) + max(scores))
-    # print("lock acquired, ", action_values)
-    action_values[action] = sum(scores) / len(scores) + max(scores)
-    # print("lock released, ", action_values)
-    # lock.release()
+    action_values[action] = sum(scores) / len(scores)
 
-def evaluate_state(env):
-    # TODO: add heuristics
-    virtual_env = gym_2048.Game2048Env()
-    scores = []
-    for i in range(explore_width):
-        virtual_env.set_board(copy.deepcopy(env.get_board()))
-        virtual_env.steps = 0
 
-        while virtual_env.steps < explore_depth:
-            # Randomly choose action in explore mode
-            if done:
-                # Only record number of legal steps
-                scores.append(virtual_env.steps)
-                break
-            action = np.random.randint(0,4)
-            next_state, _, done, info = virtual_env.step(action)
-
-        # Add additional reward if the trail does not meet end state
-        if len(scores) <= i:
-            scores.append(explore_depth * 0.5 + virtual_env.steps)
-    return sum(scores) / len(scores) + max(scores)
+# def evaluate_state(env):
+#     # TODO: add heuristics
+#     virtual_env = gym_2048.Game2048Env()
+#     scores = []
+#     for i in range(explore_width):
+#         virtual_env.set_board(copy.deepcopy(env.get_board()))
+#         virtual_env.steps = 0
+#
+#         while virtual_env.steps < explore_depth:
+#             # Randomly choose action in explore mode
+#             if done:
+#                 # Only record number of legal steps
+#                 scores.append(virtual_env.steps)
+#                 break
+#             action = np.random.randint(0,4)
+#             next_state, _, done, info = virtual_env.step(action)
+#
+#         # Add additional reward if the trail does not meet end state
+#         if len(scores) <= i:
+#             scores.append(explore_depth * 0.5 + virtual_env.steps)
+#     return sum(scores) / len(scores)
 
 def mat_to_hash(matrix):
     hash = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
+            hash *= 10
             if matrix[i][j] == 0:
                 hash += 0
             else:
-                hash += int(math.log2(matrix[i][j])) + 1
-            hash *= 13
-            hash %= 10**9 + 7
+                hash += int(math.log2(matrix[i][j])) % 10
     return hash
